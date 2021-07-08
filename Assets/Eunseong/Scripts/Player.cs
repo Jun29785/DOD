92b1aa53d93317bc0c3024ee6c5ab6e24e3c5188
EunSung <82389915+EnSung@users.noreply.github.com>
2021-07-08 12:32:41 +0900

    개인폴더

---
+++ Assets/Eunseong/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    float Hp = 3;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(new Vector2(transform.position.x,transform.position.y + 0.5f), Vector2.right);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D hitinfo = Physics2D.Raycast(new Vector2(transform.position.x,transform.position.y +0.5f), Vector2.right, 0.5f);
+
+        if(hitinfo.collider != null)
+        {
+            BattleManager.instance.isBattleP = true;
+        }
+        else
+        {
+            BattleManager.instance.isBattleP = false;
+        }
+        
+    }
+
+    public void Damage(float value)
+    {
+        Hp -= value;
+        Debug.Log(this.Hp);
+    }
+}
