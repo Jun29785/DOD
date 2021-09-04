fc270e064a218584cf2cbd5e8008aefc02a7d92f
EunSung <82389915+EnSung@users.noreply.github.com>
2021-09-05 02:17:42 +0900

    [Update] RankingUI

---
+++ Assets/Project/GameScene/Scripts/CoolTImePanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolTImePanel : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (transform.childCount != 0)
+        {
+            for (int i = 0; i <= transform.childCount; i++)
+            {
+                transform.GetChild(i).localScale = new Vector2(transform.localScale.x - 20, transform.localScale.x - 20);
+            }
+        }
+    }
+}
