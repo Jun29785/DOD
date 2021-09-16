8dc983d1a7a33391ec6a048bda0cb24b3929461d
Jun29785 <leejun9334@gmail.com>
2021-09-16 16:50:29 +0900

    [ADD] SoundManager

---
+++ Assets/Project/GameScene/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    public AudioSource bgSound;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    public void SFXPlay(string sfxName, AudioClip clip)
+    {
+        GameObject go = new GameObject(sfxName + "Sound");
+        AudioSource audioSource = go.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.Play();
+
+        Destroy(go, clip.length);
+    }
+
+    public void BgSound(AudioClip clip)
+    {
+        bgSound.clip = clip;
+        bgSound.loop = true;
+        bgSound.volume = 0.1f;
+        bgSound.Play();
+    }
+}
